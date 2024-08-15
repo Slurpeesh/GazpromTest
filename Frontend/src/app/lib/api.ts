bd71743673e83aa781313f6728712763f778c916
Slurpeesh <JeffNFG@mail.ru>
2024-08-15 22:51:30 +0300

    docker fix

--- Frontend/src/app/lib/api.ts
+++ Frontend/src/app/lib/api.ts
@@ -1,13 +1,16 @@
 import { Data } from '../config/columns'
 
+const URL =
+  process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : '/api'
+
 export async function getData(page: number): Promise<Data[]> {
-  const response = await fetch(`http://localhost:5000/?page=${page}`)
+  const response = await fetch(`${URL}/?page=${page}`)
   const data = await response.json()
   return data
 }
 
 export async function getPagesAmount() {
-  const response = await fetch('http://localhost:5000/pagesAmount')
+  const response = await fetch(`${URL}/pagesAmount`)
   const data = await response.json()
   return data
 }
